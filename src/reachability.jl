4c065adb245d40ff7fc27cc3a514233ff2eacbbf
ga74kud <michaelhartmann10@googlemail.com>
2021-01-26 20:30:07 +0100

    my second commit working with zonotypes

--- src/reachability.jl
+++ src/reachability.jl
@@ -1,14 +1,22 @@
-
-
-zonotype={
-    'c', [
+include("zonotypes.jl")
+@info "matrices"
+c=[
         1 0; 
         0 1; 
         0 0;
-        0 0], 'g': [
-        1 0; 
-        0 1; 
-        0 0;
-        0 0]}
+        0 0]
+g=[
+            1 0; 
+            0 1; 
+            0 0;
+            0 0]
+zono_a=zonotype(c, g)
+print(zono_a)
 
-print(zonotype)
+
+            
+            
+            
+            
+            
+            
---
+++ src/zonotypes.jl
@@ -0,0 +1,5 @@
+@info "struct zonotype"
+struct zonotype
+    c::Matrix
+    g::Matrix
+end
